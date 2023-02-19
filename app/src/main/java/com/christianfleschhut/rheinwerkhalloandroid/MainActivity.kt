8ca99551bec9863b049ec16ee8af9a7b3176c7fd
Christian Fleschhut <weltrekordhalter@gmail.com>
2023-02-19 12:38:12 +0100

    Set TextView to EditText content on button click

--- app/src/main/java/com/christianfleschhut/rheinwerkhalloandroid/MainActivity.kt
+++ app/src/main/java/com/christianfleschhut/rheinwerkhalloandroid/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.christianfleschhut.rheinwerkhalloandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+private var firstClick = true
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var message: TextView
     private lateinit var nextFinish: Button
+    private lateinit var input: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +21,21 @@ class MainActivity : AppCompatActivity() {
 
         message = findViewById(R.id.message)
         nextFinish = findViewById(R.id.next_finish)
+        input = findViewById(R.id.input)
 
         message.setText(R.string.welcome)
         nextFinish.setText(R.string.next)
+
+        nextFinish.setOnClickListener {
+            if (firstClick) {
+                message.text = getString(R.string.hello, input.text)
+                input.visibility = View.INVISIBLE
+
+                nextFinish.setText(R.string.finish)
+                firstClick = false
+            } else {
+                finish()
+            }
+        }
     }
 }
