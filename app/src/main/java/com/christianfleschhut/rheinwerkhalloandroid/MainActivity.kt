cf6307494236f217afab61253690fe1cd1fc5df9
Christian Fleschhut <weltrekordhalter@gmail.com>
2023-02-19 11:23:32 +0100

    Add text, text input, button views and set text

--- app/src/main/java/com/christianfleschhut/rheinwerkhalloandroid/MainActivity.kt
+++ app/src/main/java/com/christianfleschhut/rheinwerkhalloandroid/MainActivity.kt
@@ -2,10 +2,22 @@ package com.christianfleschhut.rheinwerkhalloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var message: TextView
+    private lateinit var nextFinish: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        message = findViewById(R.id.message)
+        nextFinish = findViewById(R.id.next_finish)
+
+        message.setText(R.string.welcome)
+        nextFinish.setText(R.string.next)
     }
 }
